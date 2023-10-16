f2e7edba9575ba62521dcaaba541a565db9c1826
OlafBodzioch <226356@student.uek.krakow.pl>
2023-10-16 21:59:55 +0200

    16-10-2023

---
+++ 02-ClassesAndObjects/zad9.java
@@ -0,0 +1,15 @@
+public class zad9 {
+
+    public static void main(String[] args) {
+    
+        String Name = "Olaf";
+        String Surname = "Bodzioch";
+        int Age = 21;
+        String StudentId = "226356";
+        boolean IsFirstYear = false;
+        String FieldOfStudy = "Applied Informatics";
+        String NameOfUni = "Cracow University of Economics";
+
+        System.out.println(Name + "\n" + Surname + "\n" + Age + "\n" + StudentId + "\n" + IsFirstYear + "\n" + FieldOfStudy + "\n" + NameOfUni);
+    }
+}    
